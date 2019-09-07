94318ecab086ee4ae12ab18d10953ce0014196c3
thethirdheat <emarson.serrano@gmail.com>
2019-09-06 18:42:33 -0700

    Configure User Profile

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -2272,46 +2272,108 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 
 
-var Greeting = function Greeting(props) {
-  /* note tahte its porops.history.push to chagne the page basically nooooo match lul*/
-  var onSend = function onSend(event) {
-    return props.logout() //.then(res=>props.history.push('/wtfitsowrking??'))
-    ;
-  };
-  /*<h2 className="greeting_username">Hi, {props.currentUser.username}!</h2> */
 
-  /*className="Nav--Greeting"*/
+var Greeting =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(Greeting, _React$Component);
+
+  function Greeting(props) {
+    var _this;
+
+    _classCallCheck(this, Greeting);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Greeting).call(this, props)); //this.onSend=this.onSend.bind(this)
+
+    _this.state = {
+      profileOn: false
+    };
+    return _this;
+  } //    onSend(){ 
+  //        this.props.logout()//.then(res=>props.history.push('/wtfitsowrking??'))
+  //    }
+  //
 
 
-  var withUser = function withUser() {
-    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-      className: "center"
-    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-      className: "greeting--container"
-    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-      className: "Greeting--NameContainer"
-    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-      className: "Greeting--Logout",
-      onClick: onSend
-    }, "Log", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), "Out"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-      className: "Greeting-Img",
-      src: "https://i.imgur.com/S5cgOk5.png"
-    })));
-  };
+  _createClass(Greeting, [{
+    key: "render",
+    value: function render() {
+      var _this2 = this;
 
-  var noUser = function noUser() {
-    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("nav", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
-      to: "users/new"
-    }, "Sign Up"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
-      to: "/login"
-    }, "Sign In"));
-  };
+      /* note tahte its porops.history.push to chagne the page basically nooooo match lul*/
 
-  return props.currentUser ? withUser() : noUser();
-};
+      /*<h2 className="greeting_username">Hi, {props.currentUser.username}!</h2> */
+
+      /*className="Nav--Greeting"*/
+      var profile = "";
+
+      if (this.state.profileOn && this.props.currentUser) {
+        profile = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+          className: "Greeting--Profile"
+        }, "hi bitch hi?", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+          className: "",
+          val: "onClick={()=>this.props.logout()}"
+        }, "this si the log out buton?"));
+      }
+
+      var withUser = function withUser() {
+        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+          className: "center"
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+          className: "greeting--container"
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+          className: "Greeting--NameContainer"
+        }, "wut"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+          className: "Greeting--ProfileContainer",
+          onClick: function onClick() {
+            return _this2.setState({
+              profileOn: !_this2.state.profileOn
+            });
+          }
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+          className: "Greeting-Img",
+          src: "https://i.imgur.com/S5cgOk5.png"
+        }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+          className: "Greeting--ProfileToggle"
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+          className: "fas fa-sort-down"
+        }))), profile));
+      };
+
+      var noUser = function noUser() {
+        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("nav", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+          to: "users/new"
+        }, "Sign Up"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+          to: "/login"
+        }, "Sign In"));
+      };
+
+      return this.props.currentUser ? withUser() : noUser();
+    }
+  }]);
+
+  return Greeting;
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
 
 /* harmony default export */ __webpack_exports__["default"] = (Greeting);
 
--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Greeting =(props)=>{
+class Greeting extends React.Component{
+    constructor(props){
+        super(props)
+        //this.onSend=this.onSend.bind(this)
+        this.state={
+            profileOn:false
+        }
+    }
+//    onSend(){ 
+//        this.props.logout()//.then(res=>props.history.push('/wtfitsowrking??'))
+//    }
+//
+
+    render(){
     /* note tahte its porops.history.push to chagne the page basically nooooo match lul*/
-    const onSend = (event)=>( 
-        props.logout()//.then(res=>props.history.push('/wtfitsowrking??'))
-    )
 
                     /*<h2 className="greeting_username">Hi, {props.currentUser.username}!</h2> */
 /*className="Nav--Greeting"*/
+        let profile=""
+
+
+    if(this.state.profileOn&& this.props.currentUser){
+        profile=(<div className="Greeting--Profile">
+            hi bitch hi?
+                <div className="" val="onClick={()=>this.props.logout()}">this si the log out buton?</div >
+            </div>)
+
+    }
+
     const withUser =()=>(
         <div className="center">
             <div className="greeting--container"> 
 
 
                 <div className="Greeting--NameContainer">
+                    wut
                 </div>
-                <div className="Greeting--Logout" onClick={onSend}>Log<br/>Out</div >
-                <img className="Greeting-Img" src="https://i.imgur.com/S5cgOk5.png"/>
+                
+                <div className="Greeting--ProfileContainer" onClick={()=>this.setState({profileOn:!this.state.profileOn})}>
+                    <img className="Greeting-Img" src="https://i.imgur.com/S5cgOk5.png"/>
+                    <div className="Greeting--ProfileToggle" ><i className="fas fa-sort-down"></i></div >
+                </div>
+                {profile}
             </div>
         </div> 
     )
@@ -28,7 +54,8 @@ const Greeting =(props)=>{
             <Link to ="/login">Sign In</Link> 
         </nav> 
     )
-    return props.currentUser? withUser() : noUser()
+    return this.props.currentUser? withUser() : noUser()
+}
 }
 
 export default Greeting;
